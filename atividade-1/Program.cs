c8533ac5cbd543e114df97434c0ff59e319e367b
LuizHen527 <125361452+LuizHen527@users.noreply.github.com>
2023-03-30 21:54:51 -0300

    Add files via upload

---
+++ atividade-1/Program.cs
@@ -0,0 +1,31 @@
+﻿// - Insira nota1
+// - Insira nota2
+// - Insira nota3
+// - Insira nota4
+// - Insira nota5
+// - media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+
+int nota1;
+int nota2;
+int nota3;
+int nota4;
+int nota5;
+
+Console.WriteLine("Insira a primeira nota:");
+nota1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira a segunda nota:");
+nota2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira a terceira nota:");
+nota3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira a quarta nota:");
+nota4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira a quinta nota:");
+nota5 = int.Parse(Console.ReadLine());
+
+int media = ((nota1 + nota2 + nota3 + nota4 + nota5) / 5);
+
+Console.WriteLine("Sua media é " + media);
---
+++ atividade-extra/Program.cs
@@ -0,0 +1,17 @@
+﻿// - Qual é seu ano de nascimento?
+// - idade = Input - AnoAtual
+// - semanas = idade * 52
+
+// DateOnly anoAtual = DateOnly.FromDateTime(DateTime.Now.Year);
+
+int anoAtual = DateTime.Now.Year;
+int anoNascimento;
+
+Console.WriteLine("Qual é seu ano de nascimento?");
+anoNascimento = int.Parse(Console.ReadLine());
+
+int idade = (anoAtual - anoNascimento);
+
+int semanas = (idade * 52);
+
+Console.WriteLine("Sua idade é de " + idade + " anos" + " ou " + semanas + " semanas");
